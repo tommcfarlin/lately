ccd5364fded52c3920e943c3b2fb36cb030e964a
Tom McFarlin <tom@tommcfarlin.com>
2026-04-22 14:34:41 -0400

    chore: configure commitlint and husky (#89)
    - add @commitlint/cli and @commitlint/config-conventional
    - add husky with prepare script
    - add .husky/commit-msg hook running commitlint
    - add .husky/pre-commit (empty placeholder)
    - add commitlint.config.js extending conventional with project type-enum
    
    Closes #27

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+/** @type {import('@commitlint/types').UserConfig} */
+const config = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "chore",
+        "refactor",
+        "perf",
+        "test",
+        "ci",
+        "style",
+      ],
+    ],
+  },
+};
+
+module.exports = config;
